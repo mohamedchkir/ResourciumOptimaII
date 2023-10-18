a2dd307cb6ec31ee2881ff9969aff92f413a4f9a
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-10-18 11:58:10 +0100

    remplir user et role entities avec les propriotés aussi la relation one_to_many

--- ResourciumOptimaII/src/main/java/chkir/resourciumoptimaii/entities/Role.java
+++ ResourciumOptimaII/src/main/java/chkir/resourciumoptimaii/entities/Role.java
@@ -1,4 +1,28 @@
 package chkir.resourciumoptimaii.entities;
 
+import jakarta.persistence.*;
+
+@Entity(name = "role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
