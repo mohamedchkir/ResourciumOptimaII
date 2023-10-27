129cde34aaa94b67f9127bc201d79b279428814d
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-10-27 09:22:09 +0100

    ajouter mockitto dependance et ajouter equipmentservicetest

---
+++ ResourciumOptimaII/src/test/java/chkir/resourciumoptimaii/services/EquipmentServiceTest.java
@@ -0,0 +1,20 @@
+package chkir.resourciumoptimaii.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EquipmentServiceTest {
+
+    @Test
+    void getAllEquipments() {
+    }
+
+    @Test
+    void createEquipment() {
+    }
+
+    @Test
+    void getAvailableEquipment() {
+    }
+}
